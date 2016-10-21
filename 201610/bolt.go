bc67067d6d427a40160d89f60489e191959e2a64
Dave Gray <yargevad+github@gmail.com>
2016-10-21 13:35:48 -0600

    use what we're given, not the global by accident; break out list of buckets to init

--- 201610/bolt.go
+++ 201610/bolt.go
@@ -7,17 +7,19 @@ import (
 	"github.com/pkg/errors"
 )
 
+var boltBuckets = []string{"users", "polls"}
+
 func (e *Env) boltOpen(file string) error {
 	var err error
 	var opt = bolt.Options{Timeout: 1 * time.Second}
-	env.DB, err = bolt.Open(file, 0600, &opt)
+	e.DB, err = bolt.Open(file, 0600, &opt)
 	if err != nil {
 		return errors.Wrap(err, "boltdb open failed")
 	}
 
-	err = env.DB.Update(func(tx *bolt.Tx) error {
-		// create buckets (users, polls)
-		for _, name := range []string{"users", "polls"} {
+	err = e.DB.Update(func(tx *bolt.Tx) error {
+		// create buckets
+		for _, name := range boltBuckets {
 			if _, err := tx.CreateBucketIfNotExists([]byte(name)); err != nil {
 				return errors.Wrap(err, "boltdb bucket creation failed")
 			}
