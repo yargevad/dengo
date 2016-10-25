5582721251eb111a9d3a0fe350dd7bfcaba8bbec
Dave Gray <yargevad+github@gmail.com>
2016-10-24 18:50:13 -0600

    remove commented error

--- 201610/index.go
+++ 201610/index.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	_ "github.com/pkg/errors"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +11,6 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		e := &Error{500, err}
 		e.Write(w, r)
 	}
-
-	//e := &Error{403, errors.New("foo")}
-	//e.Write(w, r)
 
 	fmt.Fprintf(w, "%v+", polls)
 }
